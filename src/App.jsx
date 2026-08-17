0e55b803b0b2164438b7e07c6747f101902f3811
rohan0752 <rohanprajapati453@gmail.com>
2026-08-17 23:43:05 +0530

    Added Parctical 2

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,33 @@ function App() {
   }
 
   if (path === "/practical-2") {
-    return <Student />;
-  }
+  return (
+    <div>
+      <h1>Student Details</h1>
+
+      <Student
+        name="Rahul Kumar"
+        rollNo="101"
+        course="BCA"
+        marks="85%"
+      />
+
+      <Student
+        name="Priya Singh"
+        rollNo="102"
+        course="BCA"
+        marks="92%"
+      />
+
+      <Student
+        name="Amit Sharma"
+        rollNo="103"
+        course="BCA"
+        marks="78%"
+      />
+    </div>
+  );
+}
 
   if (path === "/practical-3") {
     return <Counter />;
--- src/PRACTICAL 2/Student.jsx
+++ src/PRACTICAL 2/Student.jsx
@@ -4,6 +4,7 @@ function Student(props) {
   return (
     <div>
       <h2>Student Information</h2>
+
       <p><strong>Name:</strong> {props.name}</p>
       <p><strong>Roll No:</strong> {props.rollNo}</p>
       <p><strong>Course:</strong> {props.course}</p>
