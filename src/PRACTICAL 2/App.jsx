4cd40d08975493e9888015e6631a05067770d31d
rohan0752 <rohanprajapati453@gmail.com>
2026-08-17 23:34:51 +0530

    Added Practical 2

--- src/PRACTICAL 2/App.jsx
+++ src/PRACTICAL 2/App.jsx
@@ -1,11 +1,31 @@
 import React from "react";
-import Counter from "./PRACTICAL 3/Counter";
+import Student from "./Student";
 
 function App() {
   return (
     <div>
-      <h1>Counter App</h1>
-      <Counter />
+      <h1>Student Details</h1>
+
+      <Student
+        name="Rahul Kumar"
+        rollNo="101"
+        course="BCA"
+        marks="85%"
+      />
+
+      <Student
+        name="Priya Singh"
+        rollNo="102"
+        course="BCA"
+        marks="92%"
+      />
+
+      <Student
+        name="Amit Sharma"
+        rollNo="103"
+        course="BCA"
+        marks="78%"
+      />
     </div>
   );
 }
